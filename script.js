34ac87e8bfacb83a741d1d1a7aca9c5690d46e21
Megan Healey <meganhealey@mac.mynetworksettings.com>
2025-10-01 16:37:37 -0400

    Implement Generate Joke

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 const jokeEl = document.getElementById('joke');
-const jokeBtn = document.getElementById('jokeBtn');
+const jokeBtn = document.getElementById('joke-btn');
 
-function generateJoke() {
+async function generateJoke() {
    const config = {
      headers: {
        Accept: 'application/json',
      },
    };
-
-   fetch('https://icanhazdadjoke.com', config)
-     .then((res) => res.json())
-     .then((data) => {
-     jokeEl.innerHTML = data.joke;
-     });
- }
+   const res = await fetch('https://icanhazdadjoke.com', config);
+   const data = await res.json();
+   console.log(data);
+   jokeEl.innerHTML = data.joke;
+}
 
 generateJoke();
-jokeBtn.addEventListener('click', generateJoke);
